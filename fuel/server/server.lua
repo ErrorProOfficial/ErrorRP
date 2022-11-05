3ea022e7718a92e1c6c76510a8502ec2862343a9
ErrorProOfficial <112209993+ErrorProOfficial@users.noreply.github.com>
2022-11-05 23:44:03 +0200

    Update server.lua

--- fuel/server/server.lua
+++ fuel/server/server.lua
@@ -4,11 +4,11 @@ TriggerEvent('TurboCore:GetObject', function(obj) TurboCore = obj end)
 
 -- Code
 
-TurboCore.Functions.CreateCallback("turbo-fuel:server:get:fuel:config", function(source, cb)
+TurboCore.Functions.CreateCallback("fuel:server:get:fuel:config", function(source, cb)
     cb(Config)
 end)
 
-TurboCore.Functions.CreateCallback('turbo-fuel:server:can:fuel', function(source, cb, price)
+TurboCore.Functions.CreateCallback('-fuel:server:can:fuel', function(source, cb, price)
     local CanFuel = false
     local Player = TurboCore.Functions.GetPlayer(source)
     if Player.Functions.RemoveMoney("cash", price, "car-wash") then
@@ -19,14 +19,14 @@ TurboCore.Functions.CreateCallback('turbo-fuel:server:can:fuel', function(source
     cb(CanFuel)
 end)
 
-RegisterServerEvent('turbo-fuel:server:register:fuel')
-AddEventHandler('turbo-fuel:server:register:fuel', function(Plate, Vehicle, Amount)
+RegisterServerEvent('fuel:server:register:fuel')
+AddEventHandler('fuel:server:register:fuel', function(Plate, Vehicle, Amount)
     Config.VehicleFuel[Plate] = Amount
-    TriggerClientEvent('turbo-fuel:client:register:vehicle:fuel', -1, Plate, Vehicle, Amount)
+    TriggerClientEvent('fuel:client:register:vehicle:fuel', -1, Plate, Vehicle, Amount)
 end)
 
-RegisterServerEvent('turbo-fuel:server:update:fuel')
-AddEventHandler('turbo-fuel:server:update:fuel', function(Plate, Vehicle, Amount)
+RegisterServerEvent('fuel:server:update:fuel')
+AddEventHandler('fuel:server:update:fuel', function(Plate, Vehicle, Amount)
     Config.VehicleFuel[Plate] = Amount
-    TriggerClientEvent('turbo-fuel:client:update:vehicle:fuel', -1, Plate, Vehicle, Amount)
+    TriggerClientEvent('fuel:client:update:vehicle:fuel', -1, Plate, Vehicle, Amount)
 end)
