c292c8793ea1e565ff2225b538d8fc9cc9fa7841
ErrorProOfficial <112209993+ErrorProOfficial@users.noreply.github.com>
2022-11-19 21:16:24 +0200

    Add files via upload

---
+++ twitchbot/bot.js
@@ -0,0 +1,50 @@
+var tmi = require("tmi.js")
+var channelname = "error_pro12"
+var prefix = "!"
+
+var config = {
+    options: {
+        debug: true
+    },
+    connection: {
+        cluster: "aws",
+        reconnect: true
+    },
+    identity: {
+        username: "error_pro12",
+        // get yours at http://twitchapps.com/tmi
+        password: "oauth:470lcdc2zsig9o5hhbwgj4tvdumq36"
+    },
+    channels: [channelname]
+}
+
+var client = new tmi.client(config)
+client.connect();
+
+client.on("connected", (address, port) => {
+    client.action(channelname, "The bot has connected on" + address + ":" + port)
+})
+
+client.on("chat", (channel, user, message, self) => {
+    if (self) return;
+    if (message == "!discord") {
+        client.say(channelname, "lets join to my discord:https://discord.gg/8UZUzTkt")
+    };
+    if (self) return;
+    if (message == "!youtube") {
+        client.say(channelname, " lets dubscribe to my youtube :https://www.youtube.com/channel/UCJWf53V2utb4dnmpNB7YmWg")
+    };
+    if (self) return;
+    if (message == "!github") {
+        client.say(channelname, "my github :https://github.com/ErrorProOfficial/ErrorRP")
+    }
+    //cmd handler code
+    const args = message.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    try {
+        let commandFile = require(`./commands/${cmd}.js`)
+        commandFile.run(client, message, args, user, channel, self)
+    } catch (err) {
+        return;
+    }
+})
---
+++ twitchbot/commands/discord.js
@@ -0,0 +1,5 @@
+export function run(client,user) {
+    client.message(test).then(function () {
+        client.say(chanal, `@${user.username}, join to my disckord chanal :https://discord.gg/Z6QEBQrq `)
+    },
+}
